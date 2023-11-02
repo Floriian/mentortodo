db14edecc10e110bc1f6bc44657d75076e60f023
Floriian <lovasifloriian@outlook.hu>
2023-11-02 19:15:17 +0100

    setup bootstrap & ejs template engine.

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
+const indexRouter = require("./routes/index.router")
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
 app.use(cors());
+
+app.use(indexRouter);
 
 app.listen(PORT, () => console.log(`🚀: App is listening on ${PORT}`));

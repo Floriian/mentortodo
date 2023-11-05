9b9deb14a269b2434d20571bc795d99eaa0f1fbf
Floriian <lovasifloriian@outlook.hu>
2023-11-05 16:46:22 +0100

    update migration db reversed on local db

--- migrations/20231102175543-CreateTodosTable.js
+++ migrations/20231102175543-CreateTodosTable.js
@@ -29,6 +29,16 @@ module.exports = {
         defaultValue: false,
         allowNull: true,
       },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("NOW()"),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("NOW()"),
+      },
     });
   },
 
